b766536d72d05eeea965d866751aa0b3c7d9c1eb
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-13 20:36:14 -0500

    initialization function to run inquirer

--- index.js
+++ index.js
@@ -62,7 +62,11 @@ function writeToFile(data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    writeToFile(response);
+  });
+}
 
 // Function call to initialize app
 init();
